07409fb90f6d32e3300f789556145ac42a3c1557
Stephanie Gurung and Tony Fabery <tony@>
2016-06-27 09:57:22 -0700

    not capitalize designated words

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,17 @@
 class String
   define_method(:title_case) do
+    no_capitalization_array = ["A", "An", "And", "At", "But", "By", "For", "In", "Nor", "Of", "On", "Or", "So", "The", "To", "Up", "Yet"]
     title_array = self.split()
     title_array.each() do |word|
       word.capitalize!()
     end
-  title_array.join(" ")  
+    title_array.each() do |word|
+      no_capitalization_array.each() do |no|
+        if word.==(no)
+          word.downcase!()
+        end
+      end
+    end
+  title_array.join(" ")
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,4 +8,7 @@ describe('String#title_case') do
   it "capitalizes the first letter of a multiple word string" do
     expect('batman begins'.title_case()).to(eq('Batman Begins'))
   end
+  it "does not capitalize designated words" do
+    expect('batman and robin'.title_case()).to(eq('Batman and Robin'))
+  end
 end
